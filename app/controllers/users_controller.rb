fb4607df29c1f9c09a926afd10275759363005bc
Artem Pyatalov <Artem.Pyatalov@sjrb.ca>
2013-05-15 16:20:27 -0600

    Commit 13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: :destroy
 
   def show
     @user = User.find(params[:id])
@@ -24,6 +25,12 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = 'User destroyed.'
+    redirect_to users_path
+  end
+
   def update
     if @user.update_attributes(params[:user])
       sign_in @user
@@ -35,7 +42,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   private
@@ -51,4 +58,7 @@ class UsersController < ApplicationController
     redirect_to(root_path) unless current_user?(@user)
   end
 
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
 end
